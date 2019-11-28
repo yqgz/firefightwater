04632d91a8deb34023a8f0fdc6c0d955d0127f24
zgj12306 <2331766479@qq.com>
2019-11-28 02:14:38 +0000

    修改了公式初始化问题

--- firefightwater/admin.py
+++ firefightwater/admin.py
@@ -1,55 +1,55 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import Project, Table, Module, Column, ModuleTable, ProjectTable, ColumnDropdown, Dropdown
+
+
+# 装饰器注册Model
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    list_display = ('table_name', 'table_en_name', 'catalog', 'total')  # list
+
+
+@admin.register(Module)
+class ModuleAdmin(admin.ModelAdmin):
+    list_display = ('module_name', 'module_en_name')  # list
 
 
 class TagInline(admin.TabularInline):
     model = ProjectTable
 
 
-class TableAdmin(admin.ModelAdmin):
-    list_display = ('table_name', 'table_en_name', 'catalog', 'total')  # list
-
-
-class ModuleAdmin(admin.ModelAdmin):
-    list_display = ('module_name', 'module_en_name')  # list
-
-
+@admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('project_name', 'project_num', 'project_text', 'designer', 'proofreader', 'chief',
                     'approver', 'version', 'user')  # list
     inlines = [TagInline]
 
 
+@admin.register(Column)
 class ColumnAdmin(admin.ModelAdmin):
-    list_display = ('column_name', 'table', 'parameter', 'formula', 'c_formula', 'dropdown', 'defaultv', 'prompt', 'must',
-                    'aggregation', 'type', 'width')  # list
+    list_display = (
+        'column_name', 'table', 'parameter', 'formula', 'c_formula', 'dropdown', 'defaultv', 'prompt', 'must',
+        'aggregation', 'type', 'width')  # list
 
 
+@admin.register(ModuleTable)
 class ModuleTableAdmin(admin.ModelAdmin):
     list_display = ('module', 'table', 'have')  # list
 
 
+@admin.register(ProjectTable)
 class ProjectTableAdmin(admin.ModelAdmin):
     list_display = ('project', 'module', 'table')  # list
 
 
+@admin.register(ColumnDropdown)
 class ColumnDropdownAdmin(admin.ModelAdmin):
     list_display = ('column', 'dd_name')
 
 
+@admin.register(Dropdown)
 class DropdownAdmin(admin.ModelAdmin):
     list_display = ('dd_name', 'name', 'group', 'title')
 
 
-admin.site.register(Table, TableAdmin)
-admin.site.register(Module, ModuleAdmin)
-admin.site.register(Project, ProjectAdmin)
-admin.site.register(Column, ColumnAdmin)
-admin.site.register(ModuleTable, ModuleTableAdmin)
-admin.site.register(ProjectTable, ProjectTableAdmin)
-admin.site.register(ColumnDropdown, ColumnDropdownAdmin)
-admin.site.register(Dropdown, DropdownAdmin)
 admin.site.site_header = '消防系统计算平台'
 admin.site.site_title = '系统管理'
